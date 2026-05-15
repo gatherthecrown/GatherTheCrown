45a929753587288c0ccc6f458762daaa5e823c6c
Gather The Crown <99227687+gatherthecrown@users.noreply.github.com>
2026-05-14 23:19:13 -0400

    Add files via upload

--- dom/internal.d.ts
+++ dom/internal.d.ts
@@ -1,1 +1,3 @@
-export * from '../build/dom/internal';
+export { default as WebView } from './webview-wrapper';
+export declare const registerDOMComponent: undefined | typeof import('./dom-entry').registerDOMComponent;
+//# sourceMappingURL=internal.d.ts.map
